927f7aef71a86b2d1291706b26170128a24e68f7
Amirmahdi Tafreshi <a.tafreshi440@gmail.com>
2021-05-12 21:39:21 +0430

    Change output style

--- fbl/__main__.py
+++ fbl/__main__.py
@@ -37,20 +37,26 @@ def docx_extract_text(file_location:str):
 # extract all urls using the regular expression
 def find_urls(text:str):
     urls = [match.group() for match in re.finditer(url_regex, text)]
-    print("urls :", urls)
+    print("\033[1;49;34m->\033[0m \033[1mURLs found:\033[0m\n", '\n'.join(urls))
+    #print("=======================================")
     return urls
 
 #request to urls and check them
 def find_bad_urls(urls):
     bad_urls = []
     for url in urls:
+        print("    \033[1;49;92m==>\033[0m \033[1mCheck: \033[0m", url, end="")
         try :
             r = requests.get(url)
             if not r.ok :
                 bad_urls.append(url)
+                print(u"\033[31m x\033[0m")
+                continue
         except :
             bad_urls.append(url)
+            print(u"\033[31m x\033[0m")
             continue
+        print(u"\033[1;49;92m \u2713\033[0m")
     return bad_urls
 
 def main():
@@ -76,10 +82,11 @@ def main():
         else :
             print_err("That file type is not supported")
 
-        for bad_url in bad_urls:
-            print(bad_url)
+        print("=======================================")
 
-        print("[*] Total of bad urls:", len(bad_urls))
+        print('\033[31m->\033[0m \033[1mBad URLs:\033[0m', "\n".join(bad_urls))
+
+        print("\n\033[1m[*] Total of bad urls: \033[0m", "\033[31m"+str(len(bad_urls))+"\033[0m")
 
     except FileNotFoundError as exception:
         print_err('File[{}] does not exist: {}'.format(file_location, exception))
