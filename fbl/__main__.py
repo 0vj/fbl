35282fa769cb1d6de0478d0dae4dd3d047f6d15b
Amirmahdi Tafreshi <a.tafreshi440@gmail.com>
2021-05-18 23:45:14 +0430

    Improve and modify code based on pylint

--- fbl/__main__.py
+++ fbl/__main__.py
@@ -1,23 +1,23 @@
-import fitz
-import re
-import requests
 import argparse
+import re
+import fitz
+import requests
 import docx
 from ODTtoText import odtToText
 
 # a regular expression of URLs
-url_regex = r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=\n]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
+URL_REGEX = r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=\n]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
 
 def print_err(err):
     print('\033[31m {} \033[0m'.format(err))
 
-def txtfamily_extract_text(file_location:str):
+def txtfamily_extract_text(file_location:str) -> str:
     with open(file_location) as txt:
-         text = txt.read()
+        text = txt.read()
     return text
 
 #extract raw text from pdf
-def pdf_extract_text(file_location:str):
+def pdf_extract_text(file_location:str) -> str:
     # open the PDF file
     with fitz.open(file_location) as pdf:
         text = ""
@@ -27,7 +27,7 @@ def pdf_extract_text(file_location:str):
     return text
 
 #extract text of docx file
-def docx_extract_text(file_location:str):
+def docx_extract_text(file_location:str) -> str:
     docx_file = docx.Document(file_location)
     text = ""
     for paragraph in docx_file.paragraphs:
@@ -35,20 +35,20 @@ def docx_extract_text(file_location:str):
     return text
 
 # extract all urls using the regular expression
-def find_urls(text:str):
-    urls = [match.group() for match in re.finditer(url_regex, text)]
+def find_urls(text:str) -> list:
+    urls = [match.group() for match in re.finditer(URL_REGEX, text)]
     print("\033[1;49;34m->\033[0m \033[1mURLs found:\033[0m\n", '\n'.join(urls))
     #print("=======================================")
     return urls
 
 #request to urls and check them
-def find_bad_urls(urls):
+def find_bad_urls(urls) -> list:
     bad_urls = []
     for url in urls:
         print("    \033[1;49;92m==>\033[0m \033[1mCheck: \033[0m", url, end="")
         try :
-            r = requests.get(url)
-            if not r.ok :
+            res = requests.get(url)
+            if not res.ok :
                 bad_urls.append(url)
                 print(u"\033[31m x\033[0m")
                 continue
@@ -64,8 +64,8 @@ def main():
     parser.add_argument("-f", "--file_location", help="File location", required=True)
     file_location = parser.parse_args().file_location
     try :
-        f = open(file_location, 'r')
-        f.close()
+        with open(file_location, 'r'):
+            pass
         #if txt family files
         if file_location.endswith((".txt", ".htm", ".html", ".md")):
             bad_urls = find_bad_urls(find_urls(txtfamily_extract_text(file_location)))
