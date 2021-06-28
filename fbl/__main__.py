51ca49e1713535ab8c8863017c55c458770bfb4a
Amirmahdi <tafreshi.amirmahdi@protonmail.com>
2021-06-28 22:57:49 +0430

    Make code style better

--- fbl/__main__.py
+++ fbl/__main__.py
@@ -4,70 +4,77 @@ import fitz
 import requests
 import docx
 from ODTtoText import odtToText
-import logging 
+import logging
 
 # a regular expression of URLs
 URL_REGEX = r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=\n]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
 
+
 def log_err(err):
     logging.error('\033[31m {} \033[0m'.format(err))
 
-def txtfamily_extract_text(file_location:str) -> str:
+
+def txtfamily_extract_text(file_location: str) -> str:
     with open(file_location) as txt:
         text = txt.read()
     return text
 
-#extract raw text from pdf
-def pdf_extract_text(file_location:str) -> str:
-    # open the PDF file
+
+# Extract raw text from pdf
+def pdf_extract_text(file_location: str) -> str:
+    # Open the PDF file
     with fitz.open(file_location) as pdf:
         text = ""
         for page in pdf:
-            # extract text of each PDF page
+            # Extract text of each PDF page
             text += page.getText()
     return text
 
-#extract text of docx file
-def docx_extract_text(file_location:str) -> str:
+
+# Extract text of docx file
+def docx_extract_text(file_location: str) -> str:
     docx_file = docx.Document(file_location)
     text = ""
     for paragraph in docx_file.paragraphs:
         text += paragraph.text
     return text
 
-# extract all urls using the regular expression
-def find_urls(text:str) -> list:
+
+# Extract all urls using the regular expression
+def find_urls(text: str) -> list:
     urls = [match.group() for match in re.finditer(URL_REGEX, text)]
     print("\033[1;49;34m->\033[0m \033[1mURLs found:\033[0m\n", '\n'.join(urls))
     print("=======================================")
     return urls
 
-#request to urls and check them
+
+# Request to urls and check them
 def find_bad_urls(urls) -> list:
     bad_urls = []
     for url in urls:
         print("    \033[1;49;92m==>\033[0m \033[1mCheck: \033[0m", url, end="")
-        try :
+        try:
             res = requests.get(url)
-            if not res.ok :
+            if not res.ok:
                 bad_urls.append(url)
                 print(u"\033[31m x\033[0m")
                 continue
-        except :
+        except:
             bad_urls.append(url)
             print(u"\033[31m x\033[0m")
             continue
         print(u"\033[1;49;92m \u2713\033[0m")
     return bad_urls
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--file_location", help="File location", required=True)
     file_location = parser.parse_args().file_location
-    try :
+    try:
         with open(file_location, 'r'):
             pass
-        #if txt family files
+        # Check txt family files
         if file_location.endswith((".txt", ".htm", ".html", ".md")):
             bad_urls = find_bad_urls(find_urls(txtfamily_extract_text(file_location)))
 
@@ -80,7 +87,7 @@ def main():
         elif file_location.endswith(".odt"):
             bad_urls = find_bad_urls(find_urls(odtToText(file_location)))
 
-        else :
+        else:
             log_err("That file type is not supported")
 
         print("=======================================")
@@ -92,7 +99,7 @@ def main():
     except FileNotFoundError as exception:
         log_err('File[{}] does not exist: {}'.format(file_location, exception))
 
-    except IOError as exception :
+    except IOError as exception:
         log_err('File[{}] is not accessible: {}'.format(file_location, exception))
 
 
